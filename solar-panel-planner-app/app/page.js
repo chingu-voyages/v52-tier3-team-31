c10270786de018794c410bd0fea594cd0d019a3e
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-11-21 18:13:28 +0600

    Added herosection and how it work section

--- solar-panel-planner-app/app/page.js
+++ solar-panel-planner-app/app/page.js
@@ -1,10 +1,11 @@
-import HeroSection from "./components/HeroSection";
-
+import HeroSection from "@/components/home/HeroSection";
+import HowItWorks from "@/components/home/HowItWorks";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
+      <HowItWorks />
     </div>
   );
 }
--- solar-panel-planner-app/tailwind.config.js
+++ solar-panel-planner-app/tailwind.config.js
@@ -46,7 +46,7 @@ module.exports = {
         // foreground: "var(--foreground)",
       },
       backgroundImage: {
-        bannerImg: "url('/solar_bg.jpg)"
+        bannerImg: "url('https://images.unsplash.com/photo-1509391366360-2e959784a276?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       },
     },
   },
