1b696920da52eca9470fa190ee38356863ecb278
ahsas-sharma <sharma.ahsas@gmail.com>
2024-11-21 15:53:37 +0530

    add auth functionality

--- solar-panel-planner-app/components/layout/Navbar.js
+++ solar-panel-planner-app/components/layout/Navbar.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { auth, signIn, signOut } from "@/auth";
+import Link from "next/link";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+
   return (
     <header className="bg-white shadow-sm px-4 lg:px-8 py-4">
       <nav className="flex justify-between">
@@ -8,9 +12,39 @@ const Navbar = () => {
           ⚡️ BrightGrid
         </h1>
         <div className="flex items-center gap-5">
-          <button type="button" className="btn">
-            Admin Login
-          </button>
+          {session && session?.user ? (
+            <>
+              {console.log(`Session :${session}`)}
+              <Link href="/">
+                <span className="">💻 All Requests</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">
+                  <span className="">🏃 Logout</span>
+                </button>
+              </form>
+              <span className="bg-secondary-light text-white font-bold">
+                👋 {session?.user?.name}
+              </span>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              {console.log(`No Active session `)}
+              <button type="submit">
+                <span className="">🚪 Admin Login</span>
+              </button>
+            </form>
+          )}
         </div>
       </nav>
     </header>
