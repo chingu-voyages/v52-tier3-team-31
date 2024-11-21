20b3ebfa4abed46a466b9f88aebad7728dcda91f
ahsas-sharma <sharma.ahsas@gmail.com>
2024-11-21 13:30:39 +0530

    Add Navbar with logo and login button

---
+++ solar-panel-planner-app/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="bg-white shadow-sm px-4 py-2">
+      <nav className="flex justify-between">
+        <h1 className="tracking-tighter bg-gradient-to-r from-orange-800 via-orange-500 to-orange-900 bg-clip-text text-transparent font-extrabold text-2xl ">
+          ⚡️ BrightGrid
+        </h1>
+        <div className="flex items-center gap-5">
+          <button type="button" className="btn">
+            Admin Login
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
