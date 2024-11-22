89381cc43e7bb1c8f4d0bb754bd270a35c8229fc
ahsas-sharma <sharma.ahsas@gmail.com>
2024-11-22 10:15:09 +0530

    improve database connection handling with caching

--- solar-panel-planner-app/app/lib/db.js
+++ solar-panel-planner-app/app/lib/db.js
@@ -1,11 +1,32 @@
 import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGO_URI;
 
-export async function connectToDatabase() {
-  try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to database");
-    return connection;
-  } catch (error) {
-    console.log(`Error connecting to database: ${error}`);
-  }
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
+async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log("Db connected");
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+  return cached.conn;
+}
+
+export default connectToDatabase;
