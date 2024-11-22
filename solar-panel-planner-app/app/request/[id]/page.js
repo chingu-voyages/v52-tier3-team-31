0af08c59079ebc1f7947553d70129abdc87bf913
ahsas-sharma <sharma.ahsas@gmail.com>
2024-11-22 16:01:42 +0530

    add UI to display request data for a given request ID route

--- solar-panel-planner-app/app/request/[id]/page.js
+++ solar-panel-planner-app/app/request/[id]/page.js
@@ -3,13 +3,48 @@ import { getRequestById } from "@/app/actions/requestActions";
 
 const page = async ({ params }) => {
   const request = await getRequestById(params.id);
+  const { name, email, phone, address, requestedDate, scheduledDate, status } =
+    request.data;
+  console.log(request);
   return (
     <div>
       <h1 className="page-heading">Request Details</h1>
       {request && request.data ? (
-        <p className="page-subheading">Request ID: {request.data._id}</p>
+        <div>
+          <dl className="form-section flex flex-col gap-8 justify-center text-center">
+            <div>
+              <dt className="form-label">Name</dt>
+              <dd className="text-xl">{name}</dd>
+            </div>
+            <div>
+              <dt className="form-label">Email</dt>
+              <dd className="text-xl">{email}</dd>
+            </div>
+            <div>
+              <dt className="form-label">Phone</dt>
+              <dd className="text-xl">{phone}</dd>
+            </div>
+          </dl>
+
+          <dl className="form-section flex flex-col gap-4 text-center mt-4">
+            <div>
+              <dt className="form-label">Appointment Date</dt>
+              <dd className="text-xl">{scheduledDate.toLocaleString()}</dd>
+            </div>
+            <div>
+              <dt className="form-label">Appointment Status</dt>
+              <dd className="text-xl btn bg-secondary-light w-fit mx-auto text-white px-2 rounded-md ">
+                {status.toUpperCase()}
+              </dd>
+            </div>
+            <div>
+              <dt className="form-label">Address</dt>
+              <dd className="text-xl">{address}</dd>
+            </div>
+          </dl>
+        </div>
       ) : (
-        <p className="page-subheading">No request found for this ID</p>
+        <p className="page-subheading">No request found for ID : {params.id}</p>
       )}
     </div>
   );
