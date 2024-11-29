dd7524b323605bcde8d66cc792a4d4e654095b62
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-11-29 13:05:26 +0600

    Made one hour slot

--- solar-panel-planner-app/components/newRequst/DateSelector.jsx
+++ solar-panel-planner-app/components/newRequst/DateSelector.jsx
@@ -6,11 +6,15 @@ const DateSelector = ({ onDateSlotConfirm }) => {
   const [selectedSlot, setSelectedSlot] = useState(null);
 
   const timeSlots = [
-    { label: "8:00 AM - 10:00 AM", start: "08:00", end: "10:00" },
-    { label: "10:00 AM - 12:00 PM", start: "10:00", end: "12:00" },
-    { label: "12:00 PM - 2:00 PM", start: "12:00", end: "14:00" },
-    { label: "2:00 PM - 4:00 PM", start: "14:00", end: "16:00" },
-    { label: "4:00 PM - 6:00 PM", start: "16:00", end: "18:00" },
+    { label: "8:00 AM - 9:00 AM", start: "08:00", end: "09:00" },
+    { label: "9:00 AM - 10:00 AM", start: "09:00", end: "10:00" },
+    { label: "10:00 AM - 11:00 AM", start: "10:00", end: "11:00" },
+    { label: "11:00 AM - 12:00 PM", start: "11:00", end: "12:00" },
+    { label: "12:00 PM - 1:00 PM", start: "12:00", end: "13:00" },
+    { label: "1:00 PM - 2:00 PM", start: "13:00", end: "14:00" },
+    { label: "2:00 PM - 3:00 PM", start: "14:00", end: "15:00" },
+    { label: "3:00 PM - 4:00 PM", start: "15:00", end: "16:00" },
+    { label: "4:00 PM - 5:00 PM", start: "16:00", end: "17:00" },
   ];
 
   const handleDateChange = (e) => {
