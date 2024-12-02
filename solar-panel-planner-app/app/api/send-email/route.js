3807a59123ae1280b1de943010a6f1314dc17a09
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-12-02 08:40:28 +0600

    Removed commented code

--- solar-panel-planner-app/app/api/send-email/route.js
+++ solar-panel-planner-app/app/api/send-email/route.js
@@ -1,43 +1,8 @@
-// import nodemailer from "nodemailer";
-
 import { sendEmail } from "@/utils/mail"
-
-// export default async function handler(req, res) {
-//   if (req.method === "POST") {
-//     const { to, subject, text, html } = req.body;
-
-//     try {
-//       const transporter = nodemailer.createTransport({
-//         host: "smtp.gmail.com",
-//         port: 587,
-//         secure: false,
-//         auth: {
-//           user: "newe8258@gmail.com",
-//           pass: "pmer rxoo qgcp jaim",
-//         },
-//       });
-
-//       await transporter.sendMail({
-//         from: "BrightGrid",
-//         to,
-//         subject,
-//         text,
-//         html,
-//       });
-
-//       res.status(200).json({ message: "Email sent successfully" });
-//     } catch (error) {
-//       console.error("Error sending email:", error);
-//       res.status(500).json({ error: "Failed to send email" });
-//     }
-//   } else {
-//     res.status(405).json({ error: "Method not allowed" });
-//   }
-// }
 
 export async function POST(req) {
   try {
-    const body = await req.json(); // Parse the request body
+    const body = await req.json(); 
     const { sender, recipients, subject, message } = body;
 
     const result = await sendEmail({
