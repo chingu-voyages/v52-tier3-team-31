b2146d5339774feeb5b3734ac83c222de0e4976c
ahsas-sharma <sharma.ahsas@gmail.com>
2024-12-04 15:19:03 +0530

    add errory handling for map image saving functionality and add html2canvas package

--- solar-panel-planner-app/components/planning/export-to-pdf/PDFMapExport.jsx
+++ solar-panel-planner-app/components/planning/export-to-pdf/PDFMapExport.jsx
@@ -8,14 +8,24 @@ const PDFMapExport = ({ date }) => {
   const [mapDiv, setMapDiv] = useState(null);
 
   const saveMapImageData = async () => {
-    html2canvas(mapDiv, { allowTaint: false, useCORS: true }).then((canvas) => {
-      let imageData = canvas.toDataURL({
-        type: "image/jpeg",
-        quality: 1.0,
-      });
-      sessionStorage.setItem("map-image", imageData);
-      console.log(`Canvas saved in sessionStorage`);
-    });
+    console.log("Saving map image data");
+    setTimeout(() => {
+      html2canvas(mapDiv, { allowTaint: false, useCORS: true })
+        .then((canvas) => {
+          let imageData = canvas.toDataURL({
+            type: "image/jpeg",
+            quality: 1.0,
+          });
+          sessionStorage.setItem("map-image", imageData);
+          console.log(`Canvas saved in sessionStorage`);
+        })
+        .catch((err) =>
+          console.log(
+            "Error encountered while trying to save map as image :",
+            err.message
+          )
+        );
+    }, 1000);
   };
 
   // if mapElement is loaded, then get the div of the map
@@ -23,23 +33,19 @@ const PDFMapExport = ({ date }) => {
     if (!mapElement) {
       return;
     }
-    setMapDiv(mapElement.getDiv());
+    setMapDiv(mapElement.getDiv().children[0]);
   }, [mapElement]);
 
   // if mapDiv is loaded, then save the image
   useEffect(() => {
     if (mapDiv) {
-      setTimeout(() => {
-        saveMapImageData();
-      }, 1000);
+      saveMapImageData();
     }
   }, [mapDiv]);
 
   useEffect(() => {
     console.log("Date changed. Export for new date!");
-    setTimeout(() => {
-      saveMapImageData();
-    }, 1000);
+    saveMapImageData();
   }, [date]);
 
   return <></>;
