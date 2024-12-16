e7bef474dd9a9dd7690f5865f21bb68a40fbbc8c
ahsas-sharma <sharma.ahsas@gmail.com>
2024-12-16 08:35:29 +0530

    improve request detail UI

--- solar-panel-planner-app/app/request/[id]/page.js
+++ solar-panel-planner-app/app/request/[id]/page.js
@@ -20,9 +20,32 @@ const page = async ({ params }) => {
     <div>
       <h1 className="page-heading">Request Details</h1>
       {request && request.data ? (
-        <div className="flex flex-col  w-screen justify-center md:px-20 lg:px-40 xl:px-80">
-          <div className="flex flex-col gap-2 sm:flex-row">
-            <dl className="form-section flex flex-col gap-8 justify-center text-center">
+        <div className="flex flex-col  w-screen md:px-20 lg:px-40 xl:px-80 justify-stretch md:gap-2 mb-8">
+          <div className="flex flex-col sm:flex-row gap-2 mb-2 justify-center">
+            <div
+              className={` bg-gray-50 md:rounded-2xl p-8 text-center flex-col justify-center ${
+                status === "visited" || status === "cancelled" ? "w-full" : ""
+              }`}
+            >
+              <div className=" flex flex-col sm:flex-row gap-4 justify-between">
+                <div>
+                  <dt className="form-label">Appointment Date</dt>
+                  <dd className="text-xl">{scheduledDate.toLocaleString()}</dd>
+                </div>
+                <div>
+                  <dt className="form-label">Appointment Status</dt>
+                  <dd className="text-xl btn bg-secondary-light w-fit mx-auto text-white px-2 rounded-md ">
+                    {status.toUpperCase()}
+                  </dd>
+                </div>
+              </div>
+            </div>
+            {!(status === "visited" || status === "cancelled") && (
+              <RequestDetailsBtns session={session} requestId={_id} />
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <dl className="bg-gray-50 md:rounded-2xl p-8 flex flex-col gap-8 justify-center text-center">
               <div>
                 <dt className="form-label">Name</dt>
                 <dd className="text-xl">{name}</dd>
@@ -43,30 +66,6 @@ const page = async ({ params }) => {
             <div className="w-full">
               <RequestDetailMap address={address} />
             </div>
-          </div>
-
-          <div className="flex flex-col sm:flex-row gap-2 my-4 justify-between  ">
-            <div
-              className={`form-section text-center flex-col justify-center ${
-                (status === "visited" || status === "cancelled") ? "w-full" : ""
-              }`}
-            >
-              <div className=" flex flex-col sm:flex-row gap-4 justify-between">
-                <div>
-                  <dt className="form-label">Appointment Date</dt>
-                  <dd className="text-xl">{scheduledDate.toLocaleString()}</dd>
-                </div>
-                <div>
-                  <dt className="form-label">Appointment Status</dt>
-                  <dd className="text-xl btn bg-secondary-light w-fit mx-auto text-white px-2 rounded-md ">
-                    {status.toUpperCase()}
-                  </dd>
-                </div>
-              </div>
-            </div>
-            {!(status === "visited" || status === "cancelled") && (
-              <RequestDetailsBtns session={session} requestId={_id} />
-            )}
           </div>
         </div>
       ) : (
--- solar-panel-planner-app/components/buttons/PrimaryBtn/index.jsx
+++ solar-panel-planner-app/components/buttons/PrimaryBtn/index.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-const PrimaryBtn = ({ text, onClickFn, isDisabled=false }) => {
+const PrimaryBtn = ({ text, onClickFn, isDisabled = false }) => {
   return (
     <button
       className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-blue-300 font-medium rounded-lg 
-        text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 
-        focus:outline-none focus:ring-0 dark:focus:ring-0 ${
+        text-sm px-5 py-2.5 me-2 mb-2  focus:outline-none focus:ring-0 dark:focus:ring-0 ${
           isDisabled ? "opacity-50 cursor-not-allowed" : ""
         }`}
       onClick={onClickFn}
